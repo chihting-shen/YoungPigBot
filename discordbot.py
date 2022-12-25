f35657c76f5f4c4a5b8001674dd2d5b7ecd8a1f6
chihting-shen <ShenChihTing0911>
2022-12-25 09:45:17 +0800

    12

--- discordbot.py
+++ discordbot.py
@@ -4,28 +4,35 @@ import os
 from pprint import pprint
 # 輸入自己Bot的TOKEN碼
 TOKEN = os.environ['TOKEN']
+SRCLanguage=os.environ['SRC']
+DSTLanguage=os.environ['DST']
 
 client = discord.Client()
 
+# 起動時呼叫
 @client.event
 async def on_ready():
-    print('Login As：', client.user)
-    game = discord.Game('lol')
-    await client.change_presence(status=discord.Status.online, activity=game)
+    print('成功登入')
 
+# 收到訊息時呼叫
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    # 送信者為Bot時無視
+    if message.author.bot:
         return
-    if message.content == "test" or message.content == "Test":
-        await message.channel.send('Hi')
-
-    if message.content.startswith('!'):
-      tmp = message.content.split(" ",2)
-      if len(tmp) == 1:
-        await message.channel.send("?")
-      else:
-        await message.channel.send(tmp[1])
+    
+    if client.user in message.mentions: # @判定
+        translator = googletrans.Translator()
+        robotName = client.user.name
+        first, space, content = message.clean_content.partition('@'+robotName+' ')
+        
+        if content == '':
+            content = first
+        if translator.detect(content).lang == DSTLanguage:
+            return
+        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
+            remessage = translator.translate(content, dest='zh-tw').text
+            await message.reply(remessage) 
 
 # Bot起動
 client.run(TOKEN)
